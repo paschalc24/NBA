d820ecce0b59a4820c2ededbff9c2fb2ea6beae0
paschalc24 <pcorrigan@umass.edu>
2023-10-24 18:14:05 -0400

    Added endpoint getGamesbyTeam.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import morgan from 'morgan';
 import helmet from 'helmet';
 import winston from 'winston';
+import fs from 'fs';
 
 // To use logger, add to endpoints:
 // logger.info(`Get request for book: ${req.params.id}`);
@@ -24,6 +25,23 @@ app.use(morgan('tiny'))
 app.use(express.json())
 
 const port = process.env.PORT || 3000;
+
+app.get('/games/:teamname', async (req, res) => {
+    try {
+        const data = await fs.promises.readFile('./data/april2022.json', 'utf8');
+        let games = JSON.parse(data).games;
+        const teamName = req.params.teamname.toLowerCase()
+        games = games.filter(e => {
+                const home = e.teams.home.toLowerCase()
+                const away = e.teams.away.toLowerCase()
+                return home.includes(teamName) || away.includes(teamName) 
+            })
+        res.json(games)
+    }
+    catch (err) {
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+})
 
 app.listen(port, (err) => {
     if (!err) {
