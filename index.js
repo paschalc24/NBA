580bdc566ba1789bed67a419fd0fa9624bca6bad
paschalc24 <pcorrigan@umass.edu>
2023-10-24 23:49:00 -0400

    Added url sanitization mw to stop SQL injection.

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ import morgan from 'morgan';
 import helmet from 'helmet';
 import gamesRouter from './routes/gameRoutes.js';
 import authenticationMiddleware from './modules/authentication.js';
+import sanitizeURL from './modules/sanitizeURL.js';
 
 const app = express()
 app.use(helmet())
 app.use(morgan('tiny'))
 app.use(express.json())
 app.use(authenticationMiddleware)
+app.use(sanitizeURL)
 
 const port = process.env.PORT || 3000;
 
---
+++ modules/sanitizeURL.js
@@ -0,0 +1,13 @@
+const sanitizeURL = (req, res, next) => {
+    // string contains only characters a-zA-Z, 0-9, and '/'
+    const regex = /^[a-zA-Z0-9\/]+$/
+
+    if (!regex.test(req.url)){
+        res.status(400).json({ error: 'Bad Request.' });
+    }
+    else{
+        next();
+    }
+}
+
+export default sanitizeURL;
