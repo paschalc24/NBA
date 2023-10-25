ba13dc66ac45cbdcdc22e1a92e0db58de403a1f9
paschalc24 <pcorrigan@umass.edu>
2023-10-24 20:00:18 -0400

    Implement CRUD for games data and logging.

--- index.js
+++ index.js
@@ -26,8 +26,9 @@ app.use(express.json())
 
 const port = process.env.PORT || 3000;
 
-app.get('/games/:teamname', async (req, res) => {
+app.get('/gamesbyteam/:teamname', async (req, res) => {
     try {
+        logger.info(`Get request for games: ${req.params.teamname}`);
         const data = await fs.promises.readFile('./data/april2022.json', 'utf8');
         let games = JSON.parse(data).games;
         const teamName = req.params.teamname.toLowerCase()
@@ -42,6 +43,90 @@ app.get('/games/:teamname', async (req, res) => {
         res.status(500).json({ error: 'Internal Server Error' });
     }
 })
+
+app.get('/games', async (req, res) => {
+    try {
+        logger.info(`Get request for all games`);
+        const data = await fs.promises.readFile('./data/april2022.json', 'utf8');
+        let games = JSON.parse(data).games;
+        res.json(games)
+    }
+    catch (err) {
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+})
+
+app.get('/games/:id', async (req, res) => {
+    try {
+        logger.info(`Get request for game with id: ${req.params.id}`);
+        const data = await fs.promises.readFile('./data/april2022.json', 'utf8');
+        let games = JSON.parse(data).games;
+        const id = parseInt(req.params.id)
+        games = games.filter(e => e.id === id)
+        res.json(games)
+    }
+    catch (err) {
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+})
+
+app.post('/games', async (req, res) => {
+    logger.info(`Post request for game: ${JSON.stringify(req.body)}`);
+    try {
+        const data = await fs.promises.readFile('./data/april2022.json', 'utf8');
+        const gamesData = JSON.parse(data);
+        let games = gamesData.games
+        let newGame = req.body;
+        if (!newGame.teams || 
+            !newGame.points || 
+            !newGame.totalScore) {
+                res.status(400).json({ error: 'Bad Request' });
+        }
+        newGame.id = games[games.length-1].id + 1;
+        gamesData.games.push(newGame);
+        await fs.promises.writeFile('./data/april2022.json', JSON.stringify(gamesData, null, 4), 'utf8');
+        res.status(201).json(newGame);
+    } catch (err) {
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+app.put('/games/:id', async (req, res) => {
+    logger.info(`Put request for game with id: ${req.params.id} ${JSON.stringify(req.body)}`);
+    try {
+        const data = await fs.promises.readFile('./data/april2022.json', 'utf8');
+        const gamesData = JSON.parse(data);
+        const updatedGame = req.body;
+        const gameIndex = gamesData.games.findIndex(game => game.id === parseInt(req.params.id));
+        if (gameIndex === -1) {
+            res.status(404).json({ error: 'Game not found' });
+        } else {
+            gamesData.games[gameIndex] = updatedGame;
+            await fs.promises.writeFile('./data/april2022.json', JSON.stringify(gamesData, null, 4), 'utf8');
+            res.status(200).json(updatedGame);
+        }
+    } catch (err) {
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+app.delete('/games/:id', async (req, res) => {
+    logger.info(`Delete request for game: ${JSON.stringify(req.params.id)}`);
+    try {
+        const data = await fs.promises.readFile('./data/april2022.json', 'utf8');
+        const gamesData = JSON.parse(data);
+        const gameIndex = gamesData.games.findIndex(game => game.id === parseInt(req.params.id));
+        if (gameIndex === -1) {
+            res.status(404).json({ error: 'Game not found' });
+        } else {
+            gamesData.games.splice(gameIndex, 1);
+            await fs.promises.writeFile('./data/april2022.json', JSON.stringify(gamesData, null, 4), 'utf8');
+            res.status(204).send();
+        }
+    } catch (err) {
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
 
 app.listen(port, (err) => {
     if (!err) {
