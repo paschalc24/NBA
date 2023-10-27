e39a6cd656094a71b5ad2f2f938e23b2bb22f34e
paschalc24 <pcorrigan@umass.edu>
2023-10-26 23:05:44 -0400

    Added - to regex string to account for uuids.

--- modules/sanitizeURL.js
+++ modules/sanitizeURL.js
@@ -1,6 +1,6 @@
 const sanitizeURL = (req, res, next) => {
     // string contains only characters a-zA-Z, 0-9, and '/'
-    const regex = /^[a-zA-Z0-9\/]+$/
+    const regex = /^[a-zA-Z0-9\/-]+$/;
 
     if (!regex.test(req.url)){
         res.status(400).json({ error: 'Bad Request.' });
