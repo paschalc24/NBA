6fc75e8fb621f7a35ba12ae6563e0b4603882a6a
paschalc24 <pcorrigan@umass.edu>
2023-10-30 23:30:06 -0400

    Refactored post endpoint to accept optional params

--- modules/mvLinearRegression.js
+++ modules/mvLinearRegression.js
@@ -1,34 +1,48 @@
 import MLR from "ml-regression-multivariate-linear";
+import parameterMap from "./parameterMap.js"
 
-export default function mvLinearRegression (team1games, team2games) {
-    let team1points = []
-    let team2points = []
-    let team1A = []
-    let team2A = []
-    let team1averages = Array.from({length:5}).fill(0)
-    let team2averages = Array.from({length:5}).fill(0)
+export default function mvLinearRegression (team1games, team2games, query) {
+    let team1b = [];
+    let team2b = [];
+    let team1A = [];
+    let team2A = [];
+    let indepVariableAverages1 = new Map();
+    let indepVariableAverages2 = new Map();
+    for (let key of Object.keys(query)) {
+        indepVariableAverages1.set(key, 0);
+        indepVariableAverages2.set(key, 0);
+    }
     for (let game of team1games) {
-        team1points.push([game[28]]) // dependent variable
-        team1A.push([game[12], game[14], game[17], game[19], game[24]]) //independent variables
-        team1averages[0] += game[12]
-        team1averages[1] += game[14]
-        team1averages[2] += game[17]
-        team1averages[3] += game[19]
-        team1averages[4] += game[24]
+        team1b.push([game[28]]) // dependent variable
+        let A = [];
+        for (let key of indepVariableAverages1.keys()) {
+            let index = parameterMap[key];
+            A.push(game[index]);
+            indepVariableAverages1.set(key, indepVariableAverages1.get(key) + game[index]);
+        }
+        team1A.push(A);
     }
-    team1averages = team1averages.map(avg => avg/team1games.length)
-    for (let game of team2games) {
-        team2points.push([game[28]]) // dependent variable
-        team2A.push([game[12], game[14], game[17], game[19], game[24]]) //independent variables
-        team2averages[0] += game[12]
-        team2averages[1] += game[14]
-        team2averages[2] += game[17]
-        team2averages[3] += game[19]
-        team2averages[4] += game[24]
+    for (let key of indepVariableAverages1.keys()) {
+        indepVariableAverages1.set(key, indepVariableAverages1.get(key) / team1games.length)
     }
-    team2averages = team2averages.map(avg => avg/team2games.length)
 
-    const mlr1 = new MLR(team1A, team1points);
-    const mlr2 = new MLR(team2A, team2points);
+    for (let game of team2games) {
+        team2b.push([game[28]]) // dependent variable
+        let A = [];
+        for (let key of indepVariableAverages2.keys()) {
+            let index = parameterMap[key];
+            A.push(game[index]);
+            indepVariableAverages2.set(key, indepVariableAverages2.get(key) + game[index]);
+        }
+        team2A.push(A);
+    }
+    for (let key of indepVariableAverages2.keys()) {
+        indepVariableAverages2.set(key, indepVariableAverages2.get(key) / team2games.length)
+    }
+    const team1averages = Array.from(indepVariableAverages1.values());
+    const team2averages = Array.from(indepVariableAverages2.values())
+
+    const mlr1 = new MLR(team1A, team1b);
+    const mlr2 = new MLR(team2A, team2b);
     return {p0: mlr1.predict(team1averages), p1: mlr2.predict(team2averages)}
 }
